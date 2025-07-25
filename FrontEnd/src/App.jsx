c4a10ebabb2759d9b564678c8b8b18cfc5af60ba
D Ravikumara <doddamaniravi115@gmail.com>
2025-07-25 12:38:10 +0530

    Total commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Registration from './pages/Registration.jsx'
+import Home from './pages/Home.jsx'
+import DashBoard from './pages/DashBoard.jsx'
+import Login from './pages/Login.jsx'
+import Jobs from './pages/Jobs.jsx'
+import PostJobForm from './component/PostJobForm.jsx'
+import RecruiterDashboard from './component/RecruiterDashboard.jsx'
+import Company from './pages/Company.jsx'
+import RecruiterApplications from './component/RecruiterApplications.jsx'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/signup' element={<Registration />} />
+        <Route path='/dashboard' element={<DashBoard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/jobs' element={<Jobs/>}/>
+        <Route path='/postjob' element={<PostJobForm/>}/>
+        <Route path='/myposts' element={<RecruiterDashboard/>}/>
+        <Route path='/companies' element={<Company/>}/>
+        <Route path='/applicationrecived' element={<RecruiterApplications/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
